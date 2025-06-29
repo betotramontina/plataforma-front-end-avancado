9ec231c8bca64847525ee2ef007761c3cad490e4
betotramontina <122474917+betotramontina@users.noreply.github.com>
2025-06-29 19:51:29 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
 import Products from './pages/Products'
+import Contact from './pages/Contact'
 
 export default function App() {
   return (
     <div>
-      <Products />
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/products' element={<Products />} />
+          <Route path='/contact' element={<Contact />} /> 
+        </Routes>  
+      </BrowserRouter>
     </div>
   )
 }
