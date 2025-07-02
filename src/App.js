680f771b4b18a8477de98e86277ec713632fd446
betotramontina <122474917+betotramontina@users.noreply.github.com>
2025-07-02 18:05:38 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import Products from './pages/Products'
 import Contact from './pages/Contact'
+import OngDetail from './pages/OngDetail'
+import NotFound from './pages/NotFound'
 
 export default function App() {
   return (
@@ -11,6 +13,8 @@ export default function App() {
           <Route path='/' element={<Home />} />
           <Route path='/products' element={<Products />} />
           <Route path='/contact' element={<Contact />} /> 
+          <Route path='/ong-details/:id' element={<OngDetail />} />
+          <Route path='*' element={<NotFound />} /> 
         </Routes>  
       </BrowserRouter>
     </div>
