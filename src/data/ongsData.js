58662f55bd746d24b0da5ec3c51d5fb89a1bc685
betotramontina <122474917+betotramontina@users.noreply.github.com>
2025-07-02 18:02:39 -0300

    Create ongsData.js

---
+++ src/data/ongsData.js
@@ -0,0 +1,36 @@
+import ong1 from '../assets/ong1.png';
+import ong2 from '../assets/ong2.png';
+import ong3 from '../assets/ong3.png';
+import ong4 from '../assets/ong4.png';
+
+// Exporte o array de produtos como uma exportação nomeada
+export const staticProducts = [
+  {
+    id: 1,
+    name: "ONG Esperança",
+    image: ong1,
+    description: "A ONG Esperança atua com doação de cestas básicas para famílias carentes nas comunidades do Rio de Janeiro.",
+    tooltipText: "Ajude famílias carentes com alimentos essenciais.",
+  },
+  {
+    id: 2,
+    name: "ONG Mãos Unidas",
+    image: ong2,
+    description: "A ONG Mãos Unidas doa absorventes para jovens e adolescentes carentes nas comunidades do Distrito Federal.",
+    tooltipText: "Garanta a saúde e dignidade com itens de higiene pessoal.",
+  },
+  {
+    id: 3,
+    name: "ONG Futuro Brilhante",
+    image: ong3,
+    description: "A ONG Futuro Brilhante conecta voluntários com crianças e jovens que precisam de reforço escolar no interior de São Paulo",
+    tooltipText: "Apoie a educação de crianças e jovens.",
+  },
+  {
+    id: 4,
+    name: "ONG Abraço Bom",
+    image: ong4,
+    description: "A ONG Abraço Bom realiza campanhas de doação de agasalhos pelos quatro cantos do Brasil",
+    tooltipText: "Mantenha alguém aquecido no inverno.",
+  },
+];
