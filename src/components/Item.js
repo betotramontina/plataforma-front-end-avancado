e06137abd93bc7909f52ad30512fee3852b11a23
betotramontina <122474917+betotramontina@users.noreply.github.com>
2025-07-02 18:01:09 -0300

    Update Item.js

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,3 +1,6 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
 export default function Item(props) {
     // console.log(props)
 
@@ -5,10 +8,12 @@ export default function Item(props) {
     console.log(product)
 
        return (
-        <article className="product">
-            <img src={product.image} alt={product.name} /> 
-            <p className="name-product">{product.name}</p>
-            <p className="description-product">{product.description}</p>
-        </article>
+        <Link to={`/ong-details/${product.id}`} className="product-card-link">
+            <article className="product" title={product.tooltipText}>
+                <img src={product.image} alt={product.name} /> 
+                <p className="name-product">{product.name}</p>
+                <p className="description-product">{product.description}</p>
+            </article>
+        </Link>
     );
 }
